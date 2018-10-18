c6c6bb03d425ff8ce1330770156ecb42c76aee72
hogunkee <hogunhogun@naver.com>
2018-10-18 20:39:43 +0900

    django project restart

--- songrequest/manage.py
+++ songrequest/manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "SongRequest.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "songrequest.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError:
--- songrequest/songrequest/wsgi.py
+++ songrequest/songrequest/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for SongRequest project.
+WSGI config for songrequest project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "SongRequest.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "songrequest.settings")
 
 application = get_wsgi_application()
