191ea40ccaac8fcc688b4b50272db35e93e841b0
hogunkee <hogunhogun@naver.com>
2018-10-18 19:58:29 +0900

    django project

---
+++ SongRequest/blog/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.utils import timezone
+
+# Create your models here.
+class Song(models.Model):
+    store = models.ForeignKey('blog.Store')
+    time = models.DateTimeField(default=timezone.now)
+    title = models.CharField(max_length=20)
+    singer = models.CharField(max_length=20)
+    message = models.TextField()
+    play = models.BooleanField(default=False)
+    delete = models.BooleanField(default=False)
+
+
+class Store(models.Model):
+    delay = models.IntegerField()
+    site = models.CharField(max_length=10)
