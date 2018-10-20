95fdc587e4f36c615a9fe77013755023d2854215
hogunkee <hogunhogun@naver.com>
2018-10-20 11:53:29 +0900

    migrate

--- songrequest/main/models.py
+++ songrequest/main/models.py
@@ -4,14 +4,19 @@ from django.utils import timezone
 # Create your models here.
 class Song(models.Model):
     store = models.ForeignKey('main.Store')
-    time = models.DateTimeField(default=timezone.now)
-    title = models.CharField(max_length=20)
-    singer = models.CharField(max_length=20)
-    message = models.TextField()
-    play = models.BooleanField(default=False)
-    delete = models.BooleanField(default=False)
+    created_at = models.DateTimeField(default=timezone.now)
+    title = models.CharField(max_length=50)
+    singer = models.CharField(max_length=50)
+    message = models.CharField(max_length=200)
+    is_played = models.BooleanField(default=False)
+    is_deleted = models.BooleanField(default=False)
 
 
 class Store(models.Model):
+    TYPE1 = (
+        (1, '멜론'),
+        (2, '벅스'),
+    )
+    store_name = models.CharField(max_length=50)
     delay = models.IntegerField()
-    site = models.CharField(max_length=10)
+    music_site = models.IntegerField(choices=TYPE1)
